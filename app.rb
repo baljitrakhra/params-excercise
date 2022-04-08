4d81fb6c17921b14008560fbbf933aadde81b17d
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-08 09:52:19 +0100

    Form view created

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 get '/' do
   redirect '/form'
 end
 
 get '/form' do
   erb :form
+end
+
+post '/display' do
+
 end
